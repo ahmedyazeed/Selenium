f1d0abd2e808aa932a3626d9ae34fad78e4fb4cc
ahmedyazeed86 <ahmedyazeed86@gmail.com>
2023-10-17 17:47:05 -0400

    class09HW

--- src/test/java/HW/HW09T1.java
+++ src/test/java/HW/HW09T1.java
@@ -15,20 +15,25 @@ remember to print rows from all pages of tables*/
         String url="http://35.175.58.98/table-pagination-demo.php";
         String browser="chrome";
         openBrowserAndLaunchApplication(url,browser);
-
+int counter=0;
         for (int j = 0; j <3; j++) {
             List<WebElement>  cells = driver.findElements(By.xpath("//table/tbody/tr/td[7]"));
             for (int i = 0; i < cells.size(); i++) {
                 String currentCellText = cells.get(i).getText();
                 if (currentCellText.equals("Pass")) {
+                    counter++;
                     WebElement row = driver.findElement(By.xpath("//table/tbody/tr[" +(i+1)+ "]"));
                     System.out.println(row.getText());
+
                 }
             }
             WebElement nextBtn = driver.findElement(By.xpath("//a[@class='next_link']"));
             nextBtn.click();
 
         }
+        System.out.println();
+        System.out.println("This is an extra step just for me to know that the number of employees with remarks pass is "+counter);
+
 
             }
 
